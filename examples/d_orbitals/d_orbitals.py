187de176d0a6a29e442cdecc21e4bcfc5fc20a6e
Dmitry Korotin <dmitry@korotin.name>
2026-07-26 13:18:36 +0500

    cleanup

--- examples/d_orbitals/d_orbitals.py
+++ examples/d_orbitals/d_orbitals.py
@@ -3,8 +3,10 @@ import atorvi
 # d-orbitals of Niobium (Z=41)
 outfile = atorvi.OrbitalFile("d_orbitals.xsf")
 
-outfile.add_orbital("d_{3z^2-r^2}", position=[0.0, 0, 0.0], znumber=41)
-outfile.add_orbital("d_{xz}", position=[5, 0, 0.0], znumber=41)
+outfile.add_orbital("d_{3z^2-r^2}", 
+    position=[0.0, 0, 0.0], znumber=41)
+outfile.add_orbital("d_{xz}", 
+    position=[5, 0, 0.0], znumber=41)
 outfile.add_orbital("d_{yz}", position=[10, 0, 0.0], znumber=41)
 outfile.add_orbital("d_{x^2-y^2}", position=[15, 0, 0.0], znumber=41)
 outfile.add_orbital("d_{xy}", position=[20, 0, 0.0], znumber=41)
